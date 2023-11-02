76d7d2213b81ee518381a4401c85246692286562
shivam-singh-negi <shivamsingh.comp1999@gmail.com>
2023-11-03 01:22:24 +0530

    Setup connection with the MongoDb

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose=require("mongoose");
 
 const app = express();
+require("dotenv").config()
 
 app.use(express.json());
 app.use(cors());
 
-const Port = 5000;
 
+const Port = process.env.Port||5000;
+const URI= process.env.DB_URI;
 app.listen(Port, () => {
   console.log(`Welcome to the Backend! The server is now live and listening at port ${Port}`);
 });
+
+mongoose.connect(URI)
+.then(()=>{console.log("Successfully connected with the MongoDb")})
+.catch((error)=>{console.log("Something went wrong. Failed to connected with MongoDb ",error.message)});
